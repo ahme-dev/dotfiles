a30d7e9c6d22564295baa8dc6e4ba99c3e12cbe6
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-02 20:10:48 +0300

    add herbstluftwm config

---
+++ nvim/lua/plugins/fterm.lua
@@ -0,0 +1,15 @@
+return {
+	"numToStr/FTerm.nvim",
+	config = function()
+		require("FTerm").setup({
+			border     = 'double',
+			dimensions = {
+				height = 0.9,
+				width = 0.9,
+			}
+		})
+
+		vim.keymap.set('n', '<Leader>t', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<Leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+	end,
+}
